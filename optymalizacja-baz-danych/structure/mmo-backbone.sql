9b291c68db2a91ab4871f502ba0031812f5f7719
KrzysiekGL <webmaster@unexpectd.com>
2023-04-16 15:15:31 +0000

    update db structure creation script

--- optymalizacja-baz-danych/structure/mmo-backbone.sql
+++ optymalizacja-baz-danych/structure/mmo-backbone.sql
@@ -1,59 +1,35 @@
-CREATE TABLE [player] (
-  [player_id] int PRIMARY KEY,
-  [name] nvarchar(255),
-  [created_at] [timestamp],
-  [wealth] int
-)
-GO
+CREATE TABLE player (
+	player_id INT PRIMARY KEY,
+	name varchar(32),
+	created_at TIMESTAMP,
+	wealth INT
+);
 
-CREATE TABLE [non_player_character] (
-  [npc_id] int PRIMARY KEY,
-  [name] nvarchar(255),
-  [wealth] int
-)
-GO
+CREATE TABLE non_player_character (
+	npc_id INT PRIMARY KEY,
+	name varchar(32),
+	wealth int
+);
 
-CREATE TABLE [map] (
-  [map_id] int PRIMARY KEY,
-  [name] nvarchar(255)
-)
-GO
+CREATE TABLE MAP (
+	map_id int PRIMARY KEY,
+	name varchar(32)
+);
 
-CREATE TABLE [entity_location] (
-  [entity_id] int,
-  [map_id] int
-)
-GO
+CREATE TABLE item (
+	item_id int PRIMARY KEY,
+	item_type int, --- 0 - non-tradable, 1 - consumable (could be used only once), 2 - bind type (could be used many times)
+	name varchar(32)
+);
 
-CREATE TABLE [item] (
-  [item_id] int PRIMARY KEY,
-  [type] nvarchar(255) NOT NULL CHECK ([type] IN ('consumable', 'wearable', 'tool', 'quest')),
-  [name] nvarchar(255)
-)
-GO
+CREATE TABLE entity_location (
+	entity_id int,
+	map_id int
+);
 
-CREATE TABLE [item_ownership_ledger] (
-  [owner_id] int,
-  [item_id] int,
-  [price] int,
-  [tansaction_time] timestamp
-)
-GO
-
-ALTER TABLE [entity_location] ADD FOREIGN KEY ([entity_id]) REFERENCES [player] ([player_id])
-GO
-
-ALTER TABLE [entity_location] ADD FOREIGN KEY ([entity_id]) REFERENCES [non_player_character] ([npc_id])
-GO
-
-ALTER TABLE [entity_location] ADD FOREIGN KEY ([map_id]) REFERENCES [map] ([map_id])
-GO
-
-ALTER TABLE [item_ownership_ledger] ADD FOREIGN KEY ([owner_id]) REFERENCES [player] ([player_id])
-GO
-
-ALTER TABLE [item_ownership_ledger] ADD FOREIGN KEY ([owner_id]) REFERENCES [non_player_character] ([npc_id])
-GO
-
-ALTER TABLE [item_ownership_ledger] ADD FOREIGN KEY ([item_id]) REFERENCES [item] ([item_id])
-GO
+CREATE TABLE item_ownership_ledger (
+	owner_id int,
+	item_id int,
+	transaction_price int,
+	transaction_time TIMESTAMP
+);
