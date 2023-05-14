7f1c7932b980d88a56fa472ea77012851b2ece9b
KrzysiekGL <webmaster@unexpectd.com>
2023-05-14 14:10:35 +0200

    create entity_location table; add constraints (also - the arc (xor) constraint)

--- optymalizacja-baz-danych/structure/mmo-backbone.sql
+++ optymalizacja-baz-danych/structure/mmo-backbone.sql
@@ -19,12 +19,38 @@ CREATE TABLE [map] (
 )
 GO
 
-CREATE TABLE [entity_location] (
-  [player_id] int,
-  [npc_id] int,
-  [map_id] int
-)
-GO
+-- Entity location ---------------------------------------
+create table entity_location (
+    player_id number(38, 0),
+    npc_id number(38,0),
+    map_id number(38,0)
+);
+
+alter table entity_location add constraint el_xor_player_npc check (
+    (player_id is null and npc_id is not null)
+    or (player_id is not null and npc_id is null)
+);
+
+alter table entity_location add constraint el_not_null_map check (
+    (map_id is not null)
+);
+
+alter table entity_location add constraint el_fk_player
+    foreign key (player_id)
+    references player(player_id)
+;
+
+alter table entity_location add constraint el_fk_npc
+    foreign key (npc_id)
+    references non_player_character(npc_id)
+;
+
+alter table entity_location add constraint el_fk_map
+    foreign key (map_id)
+    references map(map_id)
+;
+-- Entity location ---------------------------------------
+
 
 CREATE TABLE [item] (
   [item_id] int PRIMARY KEY,
@@ -42,16 +68,7 @@ CREATE TABLE [item_ownership_ledger] (
 )
 GO
 
-ALTER TABLE [entity_location] ADD FOREIGN KEY ([map_id]) REFERENCES [map] ([map_id])
-GO
-
 ALTER TABLE [item_ownership_ledger] ADD FOREIGN KEY ([item_id]) REFERENCES [item] ([item_id])
-GO
-
-ALTER TABLE [player] ADD FOREIGN KEY ([player_id]) REFERENCES [entity_location] ([player_id])
-GO
-
-ALTER TABLE [non_player_character] ADD FOREIGN KEY ([npc_id]) REFERENCES [entity_location] ([npc_id])
 GO
 
 ALTER TABLE [player] ADD FOREIGN KEY ([player_id]) REFERENCES [item_ownership_ledger] ([player_id])
