4b30409054e68fccdd6aa031ebf52649b9579478
KrzysiekGL <webmaster@unexpectd.com>
2023-06-04 16:32:27 +0200

    add file size (from std::filesystem) demo

--- wzorce-projektowe/lab9/main.cc
+++ wzorce-projektowe/lab9/main.cc
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <atomic>
+#include <filesystem>
 
 #include <boost/asio.hpp>
 
 static std::atomic<int> c = 0;
 
 int main(int argc, char ** argv, char ** env) {
+#ifdef THREAD_POOL
+  // Thread pool example
   std::cout << "Main thread: " << c << '\n';
 
   boost::asio::thread_pool pool(argc > 1 ? std::atoi(argv[1]) : 4);
@@ -16,6 +19,15 @@ int main(int argc, char ** argv, char ** env) {
   pool.join();
 
   std::cout << "Main thread: " << c << '\n';
+#endif /* THREAD_POOL */
+
+#ifdef FILESYS
+  // Filesystem example
+  if(argc > 1)
+    std::cout << argv[1] << " " << std::filesystem::file_size(argv[1]) << '\n';
+#endif /* FILESYS */
+
+  
 
   return 0;
 }
