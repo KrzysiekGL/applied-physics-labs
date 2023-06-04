8e3365fb14af306e5c12975fcc0ba0f6ba345c96
KrzysiekGL <webmaster@unexpectd.com>
2023-06-04 17:50:49 +0200

    add recurese dir search with pool posting

--- wzorce-projektowe/lab9/main.cc
+++ wzorce-projektowe/lab9/main.cc
@@ -1,10 +1,28 @@
+#include <ios>
 #include <iostream>
 #include <atomic>
 #include <filesystem>
+#include <string>
 
 #include <boost/asio.hpp>
 
+#ifdef THREAD_POOL
 static std::atomic<int> c = 0;
+#endif /* THREAD_POOL */
+
+void searchDir(std::filesystem::directory_iterator dirIt, boost::asio::thread_pool & pool) {
+  boost::asio::post(pool, [dirIt, &pool](){
+    for(auto dirEntry : dirIt) {
+      if(dirEntry.is_directory())
+	// execute another post to the pool with recurese searchDir
+	searchDir(std::filesystem::directory_iterator(dirEntry), pool);
+      else {
+	// search for the .txt files
+	std::cout << dirEntry << '\n';
+      }
+    }
+  });
+}
 
 int main(int argc, char ** argv, char ** env) {
 #ifdef THREAD_POOL
@@ -27,7 +45,17 @@ int main(int argc, char ** argv, char ** env) {
     std::cout << argv[1] << " " << std::filesystem::file_size(argv[1]) << '\n';
 #endif /* FILESYS */
 
-  
+  // Recursively search for all .txt files in the sandbox directory
+  // Do it using asio thread pool from the boost
+
+  boost::asio::thread_pool pool(argc>1? std::atoi(argv[1]) : 4);
+
+  const std::string pathName = argc>2 ? argv[2] : "sandbox";
+  const std::filesystem::path path(pathName);
+
+  searchDir(std::filesystem::directory_iterator(path), pool);
+
+  pool.join();
 
   return 0;
 }
